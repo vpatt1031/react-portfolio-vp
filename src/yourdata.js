883c273c13870f9c0e6a5d943dcd095fd5cb2f96
Vanessa Patterson <vpatt1031@gmail.com>
2021-04-29 11:27:34 -0400

    updated info in the work section

--- src/yourdata.js
+++ src/yourdata.js
@@ -102,7 +102,7 @@ export default {
     {
       title: "Express Note Taker", //Project Title
       para:
-        "Your assignment is to modify starter code to create an application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.", // Add Your Service Type Here
+        "This application is a Note Taker that can be used to write and save notes. This application uses an Express.js back end and will save and retrieve note data from a JSON file.", // Add Your Service Type Here
       //Project Image 
       imageSrc: expess_note_taker,
       //Project URL - Add Your Project Url Here
